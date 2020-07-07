21e8e5a3e05eb9bb779c8d1d1d821a2070be6296
Your Name <you@example.com>
2020-07-06 21:49:52 -0700

    mem-alloc-examples: code improvement, logic still not working

--- mem-alloc-examples/mem-alloc-examples/kernel.cu
+++ mem-alloc-examples/mem-alloc-examples/kernel.cu
@@ -4,32 +4,42 @@
 
 #include <stdio.h>
 
-__global__ void kernel(int * a, int * pMemAddr)
+__global__ void kernel(int * dev_a, int * pMemAddr)
 {
-    *a = 1000;
-	*pMemAddr = 1001;
+    *dev_a = 1000;
+    *pMemAddr = 1001;
 }
 
 int main()
 {
-	int * dev_a  = 0;
-	int a = 100;
+	int * dev_a;
+	int * a;
 	int memAddr = 0;
+	int stat;
 
-	printf("Size of int: %d", sizeof(int));
+	stat = cudaMalloc(&dev_a, sizeof(int));
+	a = (int*)malloc(sizeof(int));
+
+	if (a != NULL) {
+		*a = 200;
+	} else {
+		printf("Failure allocating for a...\n");
+		return 1;
+	}
+	printf("Size of int: %d.\n", sizeof(int));
 	
-	printf("a before kernel call: %u.\n", a);
+	printf("a before kernel call: %u.\n", *a);
 	printf("1. cudaMalloc example, default parameters.\n");
 	printf("dev_a host address before: 0x%08x\n", dev_a);
 	cudaMalloc((void**)&dev_a, sizeof(int));
 	printf("dev_a host address after: 0x%08x\n", dev_a);
 
-	cudaMemcpy(dev_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_a, a, sizeof(int), cudaMemcpyHostToDevice);
 	kernel <<<1, 1>>>  (dev_a, &memAddr);
 	cudaMemcpy(&a, dev_a, sizeof(int), cudaMemcpyDeviceToHost);
 
-	printf("a after kernel call: %u.\n", a);
-	printf("memAddr %08x", memAddr);
+	printf("a after kernel call: %u.\n", *a);
+	//printf("memAddr %08x", memAddr);
 
 	cudaFree(dev_a);
 	getchar();
