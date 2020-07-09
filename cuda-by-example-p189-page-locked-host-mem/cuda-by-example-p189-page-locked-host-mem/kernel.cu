1105677bee8de4ac6182ff387bcf433feb3b421e
Your Name <you@example.com>
2020-07-09 14:12:37 -0700

    cuda-by-example-p189-page-locked-host-mem: support added for cudaHostAlloc, but numbers are too great.

--- cuda-by-example-p189-page-locked-host-mem/cuda-by-example-p189-page-locked-host-mem/kernel.cu
+++ cuda-by-example-p189-page-locked-host-mem/cuda-by-example-p189-page-locked-host-mem/kernel.cu
@@ -3,18 +3,23 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
-#define SIZE (10*1024*1024)
+#define SIZE (100*1024*1024)
 
 
-float cuda_malloc_test(int size, bool up) {
+float cuda_malloc_test(int size, bool up, bool hostAlloc = false) {
 	cudaEvent_t start, stop;
 	int *a, *dev_a;
 	float elapsedTime;
 
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
-	a = (int*)malloc(size * sizeof(*a));
-	cudaMalloc((void**) &dev_a, size * sizeof(*dev_a));
+
+	if (hostAlloc) {
+		a = (int*)cudaHostAlloc((void**)&a, size * sizeof(*a), cudaHostAllocDefault);
+	} else {
+		a = (int*)malloc(size * sizeof(*a));
+	}
+	cudaMalloc((void**)&dev_a, size * sizeof(*dev_a));
 
 	cudaEventRecord(start, 0);
 	for (int i = 0; i < 100; i++) {
@@ -27,7 +32,11 @@ float cuda_malloc_test(int size, bool up) {
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&elapsedTime, start, stop);
 
-	free(a);
+	if (hostAlloc) {
+		cudaFreeHost(dev_a);
+	} else {
+		cudaFree(dev_a);
+	}
 	cudaFree(a);
 	cudaEventDestroy(start);
 	cudaEventDestroy(stop);
@@ -37,12 +46,21 @@ float cuda_malloc_test(int size, bool up) {
 int main()
 {
 	float elapsedTime;
+	
+	printf("cudaMalloc test:\n");
 	float MB = (float)100 * SIZE * sizeof(int) / 1024 / 1024;
 	elapsedTime = cuda_malloc_test(SIZE, true);
 	printf("Time using cudaMalloc(up): %3.1f ms.\n", elapsedTime);
 	printf("MB/s during copy up: %3.1f.\n", MB / (elapsedTime / 1000));
-
 	elapsedTime = cuda_malloc_test(SIZE, false);
+	printf("Time using cudaMalloc(down): %3.1f ms.\n", elapsedTime);
+	printf("MB/s during copy up: %3.1f.\n", MB / (elapsedTime / 1000));
+
+	printf("cudaHostalloc test:\n");
+	elapsedTime = cuda_malloc_test(SIZE, true, 1);
+	printf("Time using cudaMalloc(up): %3.1f ms.\n", elapsedTime);
+	printf("MB/s during copy up: %3.1f.\n", MB / (elapsedTime / 1000));
+	elapsedTime = cuda_malloc_test(SIZE, false, 1);
 	printf("Time using cudaMalloc(down): %3.1f ms.\n", elapsedTime);
 	printf("MB/s during copy up: %3.1f.\n", MB / (elapsedTime / 1000));
 }
